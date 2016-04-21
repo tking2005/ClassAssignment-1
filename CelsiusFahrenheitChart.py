caf2b02a3fc6699f55fd0b51885a6985a94426f5
Taryn King <tking2005@gmail.com>
2016-04-21 10:41:09 -0400

    Created Temp.py

---
+++ CelsiusFahrenheitChart.py
@@ -0,0 +1,19 @@
+def main():
+    CelsiusFahrenheitTemp()
+
+
+
+def CelsiusFahrenheitTemp():
+    CelsiusFahrenheitTitle="Celsius   Fahrenheit"
+    print(CelsiusFahrenheitTitle)
+    CTemp = 0
+    count = 100
+    while CTemp<count:
+        FTemp=float(1.8*CTemp+32)
+        print(CTemp,"C",end="    ")
+        print(FTemp,"F")
+        CTemp = CTemp+5
+
+
+
+main()
