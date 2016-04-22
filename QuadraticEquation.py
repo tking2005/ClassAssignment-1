deaa0c1f902de52a7723eb81e7853129ec13c4b7
Taryn King <tking2005@gmail.com>
2016-04-22 14:15:14 -0400

    Created Temp.py

--- QuadraticEquation.py
+++ QuadraticEquation.py
@@ -8,23 +8,34 @@ def getQuadraticEquation():
     while (continueProgram == True):
         print("Welcome to Quadratic Computation! Enter integer values of A,B,C, and X at the corresponding prompts.")
         continueProgram = True
-        while (continueProgram == True):
+        validInputA = False
+        validInputB = False
+        validInputC = False
+        validInputX = False
+        while(validInputA==False):
             try:
                 valueA = int(input("Enter a value for A:"))
+                validInputA=True
             except:
-                print("Didn't I say an INTEGER!");valueA = int(input("Enter a value for A:"))
+                print("Didn't I say an INTEGER!")
+        while (validInputB == False):
             try:
                 valueB = int(input("Enter a value for B:"))
+                validInputB = True
             except:
-                print("That is not an integer!"); valueB = int(input("Enter a value for B:"))
+                print("That is not an integer!")
+        while (validInputC == False):
             try:
                 valueC = int(input("Enter a value for C:"))
+                validInputC = True
             except:
-                print("Didn't I say an INTEGER!");valueC = int(input("Enter a value for C:"))
+                print("Didn't I say an INTEGER!")
+        while (validInputX == False):
             try:
                 valueX = int(input("Enter a value for X:"))
+                validInputX = True
             except:
-                print("That is not an integer!");valueX = int(input("Enter a value for X:"))
+                print("That is not an integer!")
 
             print("The following quadratic equation will compute:")
             print("(",valueA,"*",valueX,"^","2) +",valueB,"+",valueC)
