7f53a21dc52f91bf92065eb8236af21834a15aa8
Taryn King <tking2005@gmail.com>
2016-04-26 14:20:54 -0400

    Created Temp.py

--- MinMax.py
+++ MinMax.py
@@ -1,71 +1,63 @@
 def main():
+
     global iterations
     iterations = getIterations()
     global numList
-    numList(iterations)
+    numList=getNumList()
     getMin(numList)
-
+    getMax(numList)
+    sortList(numList)
+    programContinues()
 
 
 def getIterations():
     return int(input("How many numbers would you like to compare?"))
 
-def numList(iteration):
-
-    numList = []
-    for iteration in range(0,iteration):
-        num = int(input("Enter a number:"))
+def getNumList():
+    numList=[]
+    for iteration in range(0,iterations):
+        num=int(input("Enter your number:"))
         numList.append(num)
     print("Your list of numbers are:")
     print(numList)
+    return numList
 
+def getMin(numCompare):
+    numMin = numCompare[0]
+    for iteration in range(0,iterations):
+        if numMin>numCompare[iteration]:
+            numMin=numCompare[iteration]
+    print("Your min number is:")
+    print(numMin)
 
-    count = 0
-    for count in range(0,iteration+1):
-        if numList[count]>=numList[count+1]:
-            maxNum=numList[count]
+def getMax(numCompare):
+    numMax = numCompare[0]
+    for iteration in range(0, iterations):
+        if numMax < numCompare[iteration]:
+            numMax = numCompare[iteration]
+    print("Your max number is:")
+    print(numMax)
+
+def sortList(numbList):
+    sortedList=[]
+    while numbList:
+        minimum = numbList[0]
+        for sort in numbList:
+            if sort < minimum:
+                minimum = sort
+        sortedList.append(minimum)
+        numbList.remove(minimum)
+    print ("Your numbers sorted are:")
+    print (sortedList)
+
+def programContinues():
+    continueProgram = True
+    while(continueProgram==True):
+        wantToContinue=str(input("-----Press Q if you want to quit, all other input continues:"))
+        wantToContinue = wantToContinue.strip()
+        if wantToContinue == "Q" or wantToContinue == "q":
+            continueProgram = False
         else:
-            maxNum=numList[count+1]
-
-        #print(numList[count])
-        count = count + 1
-    print(maxNum)
-
-
-
-
-
-
-
-    while numList:
-        minNum = numList[0]  # arbitrary number in list
-        index=0
-        for index in range (iterations):
-            if numList[index] < numList[index+1]:
-                minNum = numList[index]
-            else:
-                minNum=minNum
-    print("The minimum number is:")
-    print(minNum)
-
-    while numList:
-        maxNum = numList[0]
-        index = 0
-
-        for index in range(len(numList)):
-            if maxNum > numList[index + 1]:
-                maxNum = numList[index]
-    print("The maximum number is:")
-    print(maxNum)
-
-
-
-'''continueProgram = True
-while (continueProgram == True):
-    wantToContinue = str(input("-----Press Q if you want to quit, all other input continues:"))
-    wantToContinue = wantToContinue.strip()
-    if wantToContinue == "Q" or wantToContinue == "q":
-        continueProgram = False
-        '''
+            main()
 
 main()
---
+++ guessing.py
@@ -0,0 +1,71 @@
+def main():
+
+    global randomNumber
+    randomNumber=getRandomNum()
+    guessRandomNumber(randomNumber)
+
+
+
+
+
+def getRandomNum():
+    import random
+    return random.randint(1,10)
+
+def guessRandomNumber (randomNumb):
+    continueProgram = True
+
+    while (continueProgram == True):
+        print("Welcome to the Number Guessing Game!!!")
+        continueProgram = True
+
+
+        print("What number am I thinking of from 1 to 10?")
+
+        guessNum=False
+        guessChance=1
+        while (guessNum==False):
+            validInput = False
+
+            while (validInput == False):
+                num = int(input("Please guess a number from 1 to 10:"))
+                if num<11 and num>0:
+                    validInput = True
+                else:
+                    print("DOPE! please guess a NUMBER from 1 to 10!")
+
+
+            if num == randomNumb:
+                    print("Hooray! You guessed the number I was thinking of!!!!")
+                    guessNum=True
+            if ((num==randomNumb+1) or num==(randomNumb-1)):
+                print("Your guess is HOT!!!...CALIENTE!!!")
+                if guessChance==3:
+                    guessNum=True
+                    print("The number I was thinking of is:")
+                    print(randomNumb)
+                guessChance = guessChance + 1
+
+            elif ((num==randomNumb+2) or num==(randomNumb-2)):
+
+                print("Your guess is WARM!")
+                if guessChance == 3:
+                    guessNum = True
+                    print("The number I was thinking of is:")
+                    print(randomNumb)
+                guessChance = guessChance + 1
+            else:
+                if num <randomNumb or num>randomNumb:
+                    print("Your guess is COLD..BRRRR")
+                if guessChance == 3:
+                    guessNum = True
+                    print("The number I was thinking of is:")
+                    print(randomNumb)
+                guessChance = guessChance + 1
+        wantToContinue = str(input("--Do you want to play again?  Press Q if you want to quit, all other input continues:"))
+        wantToContinue = wantToContinue.strip()
+        if wantToContinue == "Q" or wantToContinue == "q":
+            continueProgram = False
+
+
+main()
