629bbc29d6909c8181fc18138955ba1d4a420f36
tking2005 <tking2005@gmail.com>
2016-04-27 10:21:25 -0400

    Update guessing.py

--- guessing.py
+++ guessing.py
@@ -13,11 +13,12 @@ def getRandomNum():
     return random.randint(1,10)
 
 def guessRandomNumber (randomNumb):
+    global continueProgram
     continueProgram = True
 
     while (continueProgram == True):
         print("Welcome to the Number Guessing Game!!!")
-        continueProgram = True
+        #continueProgram = True
         print("What number am I thinking of from 1 to 10? You have 3 chances!")
         global num
         guessNum=False
@@ -29,56 +30,76 @@ def guessRandomNumber (randomNumb):
 
 
                 try:
-                    num = int(input("Please guess a number from 1 to 10:"))
-                    num > 0 and num < 11
-                    validInput = True
+                    if guessNum !=4:
+                        num = int(input("Please guess a number from 1 to 10:"))
+                        num > 0 and num < 11
+                        validInput = True
                 except:
-                    print("DOPE! please guess a NUMBER from 1 to 10!")
+                    print("Silly...I said a NUMBER from 1 to 10!")
+
+                    if guessChance == 3:
+                        guessNum = True
+                        validInput = True
+                        num=0
+                        print("Sorry!!!! The number I was thinking of is:")
+                        print(randomNumb)
+                    guessChance = guessChance + 1
 
                 '''try:
                     num >0 and num<11
                     validInput = True
                 except:
-                    print("DOPE! please guess a NUMBER from 1 to 10!")
+                    print("DOPE!...I said a NUMBER from 1 to 10!")
                     '''
 
-            if num<11 and num>0 :
+            if num<11 and num>0 or num!=int:
                 validInput = True
             else:
-                print("DOPE! please guess a NUMBER from 1 to 10!")
+                print("Silly...I said a NUMBER from 1 to 10!")
+
+                if guessChance == 3:
+                    guessNum = True
+                    print("Sorry!!!! The number I was thinking of is:")
+                    print(randomNumb)
+                guessChance = guessChance + 1
+
 
 
             if num == randomNumb:
                     print("Hooray! You guessed the number I was thinking of!!!!")
+                    print("The number I was thinking of is:")
+                    print(randomNumb)
                     guessNum=True
-            if ((num==randomNumb+1) or num==(randomNumb-1)):
+            if ((num==randomNumb+1) or num==(randomNumb-1))and (guessChance!=4):
                 print("Your guess is HOT!!!...CALIENTE!!!")
                 if guessChance==3:
                     guessNum=True
                     print("Sorry!!!! The number I was thinking of is:")
                     print(randomNumb)
-                guessChance = guessChance + 1
 
-            elif ((num==randomNumb+2) or num==(randomNumb-2)):
+
+            elif ((num==randomNumb+2) or num==(randomNumb-2))and (guessChance!=4):
 
                 print("Your guess is WARM!")
+
                 if guessChance == 3:
                     guessNum = True
                     print("Sorry!!!! The number I was thinking of is:")
                     print(randomNumb)
                 guessChance = guessChance + 1
             else:
-                if num <randomNumb or num>randomNumb:
-                    print("Your guess is COLD..BRRRR")
-                if guessChance == 3:
+                if (num <randomNumb or num>randomNumb) and (guessChance!=4):
+                    print("Sorry!!!Your guess is COLD..BRRRR")
+                if guessChance == 3 and num!=randomNumb:
                     guessNum = True
-                    print("Sorry!!!  The number I was thinking of is:")
+                    print("The number I was thinking of is:")
                     print(randomNumb)
                 guessChance = guessChance + 1
         wantToContinue = str(input("--Do you want to play again?  Press Q if you want to quit, all other input continues:"))
         wantToContinue = wantToContinue.strip()
         if wantToContinue == "Q" or wantToContinue == "q":
             continueProgram = False
+
         else:
             main()
 
